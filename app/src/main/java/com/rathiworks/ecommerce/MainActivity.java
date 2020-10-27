9fb3376212b2cbb61013eba195073ee2c47ea29d
Prakhar Rathi <prakhar@mail.uni-paderborn.de>
2020-10-28 00:44:00 +0100

    Firebase Added, Entry UI Added

--- app/src/main/java/com/rathiworks/ecommerce/MainActivity.java
+++ app/src/main/java/com/rathiworks/ecommerce/MainActivity.java
@@ -3,12 +3,20 @@ package com.rathiworks.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signUpButton;
+    private Button logInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpButton = (Button) findViewById(R.id.signup_button);
+        logInButton = (Button) findViewById(R.id.login_button);
+
     }
 }
